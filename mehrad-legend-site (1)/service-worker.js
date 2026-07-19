aa510e670ec59bab8c22709fb8ef14324c77c460
Doctoriran <mehradharatian@gmail.com>
2026-07-20 00:30:10 +0330

    Delete mehrad-legend-site (1) directory
    بب

--- mehrad-legend-site (1)/service-worker.js
+++
@@ -1,9 +0,0 @@
-
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open("mehrad-v1").then(cache => cache.addAll([
-    "./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./assets/logo.svg"
-  ])));
-});
-self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
